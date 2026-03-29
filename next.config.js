fde5aefacef135c3d9840bd7e1198c4e8dae56d1
philoulehackeur <151377941+philoulehackeur@users.noreply.github.com>
2026-03-29 17:19:17 +0200

    security: upgrade next.js and react to latest patched versions

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      'firebase-admin',
-      '@google-cloud/firestore',
-      '@grpc/grpc-js',
-      'google-gax',
-      'qrcode',
-    ],
-  },
+  serverExternalPackages: [
+    'firebase-admin',
+    '@google-cloud/firestore',
+    '@grpc/grpc-js',
+    'google-gax',
+    'qrcode',
+  ],
 };
 
 module.exports = nextConfig;
